9f6be65d954aad8fcaaea2ae573b4f1de7be4cc3
Henry Warnock <henrysworkingnow@gmail.com>
2026-07-18 12:30:33 -0700

    Revert "Updated Code"
    This reverts commit 4df14bbce14deeb03fb42d5ea09cd0df350de2df.

--- Testproj/test.cs
+++ Testproj/test.cs
@@ -2,7 +2,5 @@ var qt = "subtend";
 void Asymptote(string a)
 {
     Console.Write(a);
-    Console.WriteLine(" is a word that means to approach a line or curve but never touch it.");
 }
 Asymptote(qt);
-Console.WriteLine("Code ran properly");
