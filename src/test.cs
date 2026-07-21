1fdf47b5771637772d4a08d554fc180b6d5947a6
Henry Warnock <henrysworkingnow@gmail.com>
2026-07-20 23:29:54 -0700

    wrote new function

--- src/test.cs
+++ src/test.cs
@@ -1,6 +1,25 @@
-string qt = "subtend";
-void Asymptote(string a)
+/*
+Console.Write("degrees or radians?: ");
+string? mode = Console.ReadLine();
+
+if (mode != "radians" && mode!= "degrees")
 {
-    Console.WriteLine(a);
+    Console.WriteLine("Invalid mode! options are;'degrees' or 'radians'");
 }
-Asymptote(qt);
+int
+*/
+int nAngle(int inp)
+{
+    int PoR = (int)System.Math.Floor((double)inp / 180);
+   inp -=180*PoR;
+    return(inp);
+}
+Console.WriteLine(nAngle(730));
+
+/*
+int inputvb = 20;
+if(inputvb > 180 && inputvb < 0)
+{
+    int inputvc=nAngle(210);
+}
+*/
