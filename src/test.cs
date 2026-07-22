0f1f622655c4bf26c33fd78f605e2debaa453b29
Henry Warnock <henrysworkingnow@gmail.com>
2026-07-21 20:45:17 -0700

    wrote script

--- src/test.cs
+++ src/test.cs
@@ -1,27 +1,56 @@
 /*
-Console.Write("degrees or radians?: ");
-string? mode = Console.ReadLine();
-
-if (mode != "radians" && mode!= "degrees")
-{
-    Console.WriteLine("Invalid mode! options are;'degrees' or 'radians'");
-}
-int
+Simple sine calculator using Bhaskara's approximation, with a relatively small margin for error.
+Code runs in groups of 5 and adds one the number of degrees each iteration.
 */
+
 int nAngle(int inp)
 {
-    int PoR = (int)System.Math.Floor((double)inp / 180);
-   inp -=180*PoR;
-    inp -= 1;
-    inp += 1;
+    int PoR=1;
+    if (inp>0)
+    {
+        PoR = (int)System.Math.Floor((double)inp / 180);
+        inp-=180*PoR;
+    }
+    if (inp<0)
+    {
+        PoR = (int)System.Math.Ceiling((double)inp / 180);
+        inp-=180*PoR;
+    }
     return(inp);
 }
-Console.WriteLine(nAngle(730));
-
-/*
-int inputvb = 20;
-if(inputvb > 180 && inputvb < 0)
+float Calculation(int s)
 {
-    int inputvc=nAngle(210);
+    int minuss = 1;
+    int nmrt = 1;
+    int dnmtr = 1;
+    minuss = s*180-s*s;
+    nmrt = 4*minuss;
+    dnmtr = 40500-minuss;
+    float fin = ((float)nmrt/dnmtr);
+    if(s>180 && s<360)
+    {
+        fin *= -1;
+    }
+    return((float)fin);
 }
-*/
+int q = 1;
+while(true)
+{
+    
+    for(int i=0; i<5; i++)
+    {
+        Console.Write("Random Angle: ");
+        Console.WriteLine((int)q);
+        Console.WriteLine((float)Calculation(nAngle(q)));
+        q+=1;
+    }
+
+    Console.Write("5 iterations! resetting");
+    Console.Write(".");
+    Thread.Sleep(700);
+    Console.Write(".");
+    Thread.Sleep(700);
+    Console.Write(".");
+    Thread.Sleep(700);
+    Console.Clear();
+}
